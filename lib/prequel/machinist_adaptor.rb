0503a793b80af5bed47e10d1ca4dd1d2d9916022
Nathan Sobo <nathan@github.com>
2012-09-04 12:52:25 -0600

    Upgrade machinist adaptor to work w/ machinist 3

--- lib/prequel/machinist_adaptor.rb
+++ lib/prequel/machinist_adaptor.rb
@@ -1,86 +1,47 @@
-require 'machinist/blueprints'
+require 'machinist'
+require 'machinist/blueprint'
+require 'machinist/lathe'
 
 module Prequel
-  class MachinistAdaptor
-    class << self
-      def has_association?(object, attribute)
-        object.respond_to?("#{attribute}_id")
-      end
+  class Blueprint < Machinist::Blueprint
+    def make!(attributes = {})
+      object = make(attributes)
+      object.save!
+    end
 
-      def class_for_association(object, attribute)
-        object.send(attribute).tuple_class
-      end
-
-      def assigned_attributes_without_associations(lathe)
-        attributes = {}
-        lathe.assigned_attributes.each_pair do |attribute, value|
-          if has_association?(lathe.object, attribute)
-            if lathe.object.respond_to?("#{attribute}_id=".to_sym)
-              attributes["#{attribute}_id".to_sym] = value.id
-            end
-          else
-            attributes[attribute] = value
-          end
-        end
-        attributes
-      end
+    def lathe_class
+      Prequel::Lathe
     end
   end
 
-  Record.extend(Machinist::Blueprints::ClassMethods)
+  class Lathe < Machinist::Lathe
+  end
 
-  module MachinistRecordExtensions
-    def make(*args, &block)
-      lathe = Machinist::Lathe.run(MachinistAdaptor, self.new, *args)
-      unless Machinist.nerfed?
-        if lathe.object.valid?
-          lathe.object.save
-        else
-          raise("Save failed: #{lathe.object.validation_errors.inspect}")
-        end
+  class Record
+    extend Machinist::Machinable
+
+    def self.blueprint_class
+      Prequel::Blueprint
+    end
+  end
+
+  module Relations
+    class Selection
+      def make!(attributes={})
+        operand.make!(predicate.enhance_attributes(attributes))
       end
-      lathe.object(&block)
+
+      def make(attributes={})
+        operand.make(predicate.enhance_attributes(attributes))
+      end
     end
 
-    def make_unsaved(*args)
-      object = Machinist.with_save_nerfed { make(*args) }
-      yield object if block_given?
-      object
+    class Table
+      delegate :make!, :make, :to => :tuple_class
     end
 
-    def plan(*args)
-      lathe = Machinist::Lathe.run(MachinistAdaptor, self.new, *args)
-      MachinistAdaptor.assigned_attributes_without_associations(lathe)
+    class Relation
+      delegate :make!, :make, :to => :operand
     end
-
-    Record.extend(self)
-  end
-
-  module MachinistSelectionMixin
-    def make(attributes={})
-      operand.make(predicate.enhance_attributes(attributes))
-    end
-
-    def make_unsaved(attributes={})
-      operand.make_unsaved(predicate.enhance_attributes(attributes))
-    end
-
-    def plan(attributes={})
-      operand.plan(predicate.enhance_attributes(attributes))
-    end
-
-    Relations::Selection.send(:include, MachinistSelectionMixin)
-  end
-
-  module MachinistTableMixin
-    delegate :make, :make_unsaved, :plan, :to => :tuple_class
-
-    Relations::Table.send(:include, MachinistTableMixin)
-  end
-
-  module MachinistRelationMixin
-    delegate :make, :make_unsaved, :plan, :to => :operand
-
-    Relations::Relation.send(:include, MachinistRelationMixin)
   end
 end
